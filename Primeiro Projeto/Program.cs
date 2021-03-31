33d12222da1658900db05867514826f15581bcdd
Edison Junior <mage1232010@gmail.com>
2021-03-30 23:09:48 -0300

    Adicionei um Hello World

--- Primeiro Projeto/Program.cs
+++ Primeiro Projeto/Program.cs
@@ -28,6 +28,7 @@ namespace Primeiro_Projeto
             double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
 
             Console.WriteLine(desconto);
+            Console.WriteLine("Hello World!");
         }
     }
 }
