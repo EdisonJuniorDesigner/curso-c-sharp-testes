536804e29d0cc78d0e0a8faea2c6466e9fcb2e13
Edison Junior <mage1232010@gmail.com>
2021-03-30 23:08:03 -0300

    Teste condicional ternaria

---
+++ Primeiro Projeto/Program.cs
@@ -0,0 +1,33 @@
+﻿//Código Principal
+using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace Primeiro_Projeto
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            // Expressão Condicional If/Else
+            /*
+            double desconto;
+            if (preco < 20.0)
+            {
+                desconto = preco * 0.1;
+            }
+            else
+            {
+                desconto = preco * 0.05;
+            }
+            */
+
+            // Expressão Condicional ternária, seria no caso If/Else simplificado
+            double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+
+            Console.WriteLine(desconto);
+        }
+    }
+}
